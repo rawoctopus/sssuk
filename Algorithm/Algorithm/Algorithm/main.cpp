b45c1d8f944674cb8326c58fe2861ee6f334c341
sssukho <sssukho@gmail.com>
2019-07-03 12:16:23 +0900

    알고리즘 프로젝트 생성

---
+++ Algorithm/Algorithm/Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Sukho Lim on 03/07/2019.
+//  Copyright © 2019 Sukho Lim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
