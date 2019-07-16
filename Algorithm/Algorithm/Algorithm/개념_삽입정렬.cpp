cc2d9694a285ce8eb1fbda845638d65e083521c8
sssukho <sssukho@gmail.com>
2019-07-16 14:07:23 +0900

    Insertion Sort(삽입 정렬) 개념

---
+++ Algorithm/Algorithm/Algorithm/개념_삽입정렬.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#define MAX 5005
+//using namespace std;
+//
+//int d[MAX], n;
+//
+//int *selection_sort(int *arr, int n) {
+//    for(int i = 0; i < n; i++) {
+//        int temp = d[i]; //i=2, temp=1
+//        int j = i - 1; //j=1
+//        for(; j >= 0; j--) {
+//            if(d[j] < temp)
+//                break;
+//            d[j+1] = temp;
+//        }
+//        d[j+1] = temp;
+//    }
+//    return arr;
+//}
+//
+////int *selection_sort(int *arr, int n) {
+////    for(int i = 0; i < n; i++) {
+////        int temp = d[i];
+////
+////    }
+////
+////    return arr;
+////}
